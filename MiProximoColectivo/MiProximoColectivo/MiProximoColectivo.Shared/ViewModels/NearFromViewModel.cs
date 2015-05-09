0fac191d026cc83f37418eeefa156744e738b858
fseba <f.seba@hotmail.com>
2015-05-09 17:11:42 -0300

    Creacion de ViewModels

---
+++ MiProximoColectivo/MiProximoColectivo/MiProximoColectivo.Shared/ViewModels/NearFromViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MiProximoColectivo.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Phone.UI.Input;
+
+namespace MiProximoColectivo.ViewModels
+{
+    public class NearFromViewModel : ViewModelBase
+    {
+        public override System.Threading.Tasks.Task OnNavigatedFrom(Windows.UI.Xaml.Navigation.NavigationEventArgs args)
+        {
+            return null;
+        }
+
+        public override System.Threading.Tasks.Task OnNavigatingFrom(Windows.UI.Xaml.Navigation.NavigatingCancelEventArgs args)
+        {
+            return null;
+        }
+
+        public override System.Threading.Tasks.Task OnNavigatedTo(Windows.UI.Xaml.Navigation.NavigationEventArgs args)
+        {
+            return null;
+        }
+
+        public override System.Threading.Tasks.Task BackKeyPressed(BackPressedEventArgs args)
+        {
+            return null;
+        }
+
+        public override void CleanViewModel()
+        {
+        }
+    }
+}
